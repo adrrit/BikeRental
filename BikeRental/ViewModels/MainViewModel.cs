5955a9f5073b8cf7d7d7fefb9c398624aec63724
piotr.moskala <piotr.moskala@PIOTR-PC>
2016-07-01 11:12:43 +0200

    MainView, EventAggregator

---
+++ BikeRental/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRental.ViewModels
+{
+    public class MainViewModel : Screen
+    {
+        //uchwyt referencji event aggregatora
+        private readonly IEventAggregator _eventAggregator;
+
+        public MainViewModel(IEventAggregator eventAggregator)
+        {
+            //pobranie refernecji z eventAggregatora
+            _eventAggregator = eventAggregator;
+            _eventAggregator.Subscribe(this);
+        }
+    }
+}
--- BikeRental/ViewModels/ShellViewModel.cs
+++ BikeRental/ViewModels/ShellViewModel.cs
@@ -6,8 +6,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BikeRental.ViewModels
-{
+{    
     public class ShellViewModel : Conductor<object>
     {
+        private readonly IEventAggregator _eventAggregator;
+
+        public ShellViewModel()
+        {
+            _eventAggregator = new EventAggregator();
+            _eventAggregator.Subscribe(this);
+
+            // Show MainView on app startup
+            ShowMainView(); 
+        }
+
+        private void ShowMainView()
+        {
+            ActivateItem(new MainViewModel(_eventAggregator));
+        }
     }
 }
