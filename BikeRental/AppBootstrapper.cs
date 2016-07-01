ba75fe3b4608a08a5abaf71a61cac6f9e253b4db
piotr.moskala <piotr.moskala@PIOTR-PC>
2016-07-01 10:33:08 +0200

    Shell, mahapps, caliburn

---
+++ BikeRental/AppBootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using BikeRental.ViewModels;
+using Caliburn.Micro;
+
+namespace BikeRental
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, System.Windows.StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+        private readonly SimpleContainer _container =
+           new SimpleContainer();
+
+        // ... Other Bootstrapper Config
+
+        protected override void Configure()
+        {
+            _container.Singleton<IEventAggregator, EventAggregator>();
+        }
+    }
+}
