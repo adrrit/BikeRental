5778c9676faf0b78c62510638614064f5f7fd38e
piotr.moskala <piotr.moskala@PIOTR-PC>
2016-07-04 16:34:17 +0200

    AutoPrint

---
+++ BikeRental/ReportViewer/RPReportWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using BikeRental.Classes;
+using Microsoft.Reporting.WinForms;
+using System.Windows;
+
+namespace BikeRental.ReportViewer
+{
+    /// <summary>
+    /// Interaction logic for RPReportWindow.xaml
+    /// </summary>
+    public partial class RPReportWindow : Window
+    {
+        public RPReportWindow(bool printWithPreview)
+        {
+            InitializeComponent();
+
+            ReportDataSource reportDataSource = new ReportDataSource();
+            reportDataSource.Name = "listaObecnosci"; // Name of the DataSet we set in .rdlc
+            //reportDataSource.Value = czasPracyGastronomias;
+            reportViewer.LocalReport.ReportPath = "*.rdlc"; // Path of the rdlc file
+            reportViewer.LocalReport.DataSources.Add(reportDataSource);
+            if (printWithPreview == true)
+            {
+                reportViewer.SetDisplayMode(DisplayMode.PrintLayout);
+                reportViewer.RefreshReport();
+            }
+            else
+            {
+                AutoPrintCls autoprintme = new AutoPrintCls(reportViewer.LocalReport);
+                autoprintme.Print();
+
+                this.Close();
+            }
+        }
+
+        private void reportViewer_RenderingComplete(object sender, Microsoft.Reporting.WinForms.RenderingCompleteEventArgs e)
+        {
+
+        }
+    }
+}
